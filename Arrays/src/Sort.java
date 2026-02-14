ba01a600f996f2b1f4038ae4aa1e4e042a5bc32f
Anshul <mishraanshul559@gmail.com>
2026-02-14 16:25:58 +0530

    sorting by method

---
+++ Arrays/src/Sort.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Sort {
+    public static void main(String[] args) {
+        int[] arr = {1,5,7,8,9,6,8,4};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
